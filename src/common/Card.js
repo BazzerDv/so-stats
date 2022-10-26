c6ff318a3c4a9ffc1df96eaf60b723f1eeb6e6e1
BazzerDv <bazzerdv@gmail.com>
2022-10-26 16:39:19 +0100

    Update Card.js

--- src/common/Card.js
+++ src/common/Card.js
@@ -57,13 +57,17 @@ class Card {
   render(body) {
     return `
       <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="${this.getWidth()}" height="${this.getHeight()}" viewBox="${`0 0 ${this.getWidth()} ${this.getHeight()}`}">
+        <linearGradient id="Gradient2" x1="1" x2="0" y1="0" y2="0">
+          <stop offset="0%" stop-color="#000370"/>
+          <stop offset="50%" stop-color="#00000b" />
+        </linearGradient>
         <style>
           ${this.css}
           ${getAnimations()}
         </style>
-        <rect xmlns="http://www.w3.org/2000/svg" data-testid="card-bg" fill="${
-          this.bgColor
-        }" x="0.5" height="99%" rx="10" width="${this.getWidth()}" stroke-width="3" stroke-opacity="${
+        <rect xmlns="http://www.w3.org/2000/svg" data-testid="card-bg" fill="
+          url(#Gradient2)
+        " x="0.5" height="99%" rx="10" width="${this.getWidth()}" stroke-width="3" stroke-opacity="${
       this.hideBorder ? 0 : 1
     }" />
         <!-- Title -->
